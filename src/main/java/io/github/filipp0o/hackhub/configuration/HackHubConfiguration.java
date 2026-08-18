35a46444be4ec46c8edda18b427ec238b6a713b1
Filipp0o <filippoferretti04@gmail.com>
2026-08-18 09:50:11 +0200

    Configure Spring application components

---
+++ src/main/java/io/github/filipp0o/hackhub/configuration/HackHubConfiguration.java
@@ -0,0 +1,165 @@
+package io.github.filipp0o.hackhub.configuration;
+
+import io.github.filipp0o.hackhub.application.ConfigurareRiscossionePremioControl;
+import io.github.filipp0o.hackhub.application.CreareHackathonControl;
+import io.github.filipp0o.hackhub.application.CreareTeamControl;
+import io.github.filipp0o.hackhub.application.ErogarePremioControl;
+import io.github.filipp0o.hackhub.application.EsaminareSegnalazioneControl;
+import io.github.filipp0o.hackhub.application.HackathonRepository;
+import io.github.filipp0o.hackhub.application.PartecipazioneRepository;
+import io.github.filipp0o.hackhub.application.ProclamareTeamVincitoreControl;
+import io.github.filipp0o.hackhub.application.SegnalareViolazioneControl;
+import io.github.filipp0o.hackhub.application.SegnalazioneRepository;
+import io.github.filipp0o.hackhub.application.SistemaPagamentoGateway;
+import io.github.filipp0o.hackhub.application.TeamRepository;
+import io.github.filipp0o.hackhub.application.UtenteRepository;
+import io.github.filipp0o.hackhub.application.ValutareSottomissioneControl;
+import io.github.filipp0o.hackhub.application.ValutazioneRepository;
+import io.github.filipp0o.hackhub.infrastructure.HackathonRepositoryImpl;
+import io.github.filipp0o.hackhub.infrastructure.PartecipazioneRepositoryImpl;
+import io.github.filipp0o.hackhub.infrastructure.SegnalazioneRepositoryImpl;
+import io.github.filipp0o.hackhub.infrastructure.SistemaPagamentoAdapter;
+import io.github.filipp0o.hackhub.infrastructure.TeamRepositoryImpl;
+import io.github.filipp0o.hackhub.infrastructure.UtenteRepositoryImpl;
+import io.github.filipp0o.hackhub.infrastructure.ValutazioneRepositoryImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class HackHubConfiguration {
+
+    @Bean
+    public UtenteRepository utenteRepository() {
+        return new UtenteRepositoryImpl(List.of());
+    }
+
+    @Bean
+    public TeamRepository teamRepository() {
+        return new TeamRepositoryImpl();
+    }
+
+    @Bean
+    public PartecipazioneRepository partecipazioneRepository() {
+        return new PartecipazioneRepositoryImpl();
+    }
+
+    @Bean
+    public HackathonRepository hackathonRepository(
+            PartecipazioneRepository partecipazioneRepository
+    ) {
+        return new HackathonRepositoryImpl(
+                partecipazioneRepository
+        );
+    }
+
+    @Bean
+    public SegnalazioneRepository segnalazioneRepository() {
+        return new SegnalazioneRepositoryImpl();
+    }
+
+    @Bean
+    public ValutazioneRepository valutazioneRepository() {
+        return new ValutazioneRepositoryImpl();
+    }
+
+    @Bean
+    public SistemaPagamentoGateway sistemaPagamentoGateway() {
+        return new SistemaPagamentoAdapter();
+    }
+
+    @Bean
+    public CreareTeamControl creareTeamControl(
+            TeamRepository teamRepository
+    ) {
+        return new CreareTeamControl(teamRepository);
+    }
+
+    @Bean
+    public CreareHackathonControl creareHackathonControl(
+            UtenteRepository utenteRepository,
+            HackathonRepository hackathonRepository
+    ) {
+        return new CreareHackathonControl(
+                utenteRepository,
+                hackathonRepository
+        );
+    }
+
+    @Bean
+    public ValutareSottomissioneControl
+    valutareSottomissioneControl(
+            HackathonRepository hackathonRepository,
+            PartecipazioneRepository partecipazioneRepository,
+            ValutazioneRepository valutazioneRepository
+    ) {
+        return new ValutareSottomissioneControl(
+                hackathonRepository,
+                partecipazioneRepository,
+                valutazioneRepository
+        );
+    }
+
+    @Bean
+    public SegnalareViolazioneControl segnalareViolazioneControl(
+            HackathonRepository hackathonRepository,
+            PartecipazioneRepository partecipazioneRepository,
+            SegnalazioneRepository segnalazioneRepository
+    ) {
+        return new SegnalareViolazioneControl(
+                hackathonRepository,
+                partecipazioneRepository,
+                segnalazioneRepository
+        );
+    }
+
+    @Bean
+    public EsaminareSegnalazioneControl
+    esaminareSegnalazioneControl(
+            SegnalazioneRepository segnalazioneRepository,
+            PartecipazioneRepository partecipazioneRepository
+    ) {
+        return new EsaminareSegnalazioneControl(
+                segnalazioneRepository,
+                partecipazioneRepository
+        );
+    }
+
+    @Bean
+    public ProclamareTeamVincitoreControl
+    proclamareTeamVincitoreControl(
+            PartecipazioneRepository partecipazioneRepository,
+            HackathonRepository hackathonRepository,
+            SegnalazioneRepository segnalazioneRepository
+    ) {
+        return new ProclamareTeamVincitoreControl(
+                partecipazioneRepository,
+                hackathonRepository,
+                segnalazioneRepository
+        );
+    }
+
+    @Bean
+    public ConfigurareRiscossionePremioControl
+    configurareRiscossionePremioControl(
+            SistemaPagamentoGateway sistemaPagamentoGateway,
+            HackathonRepository hackathonRepository
+    ) {
+        return new ConfigurareRiscossionePremioControl(
+                sistemaPagamentoGateway,
+                hackathonRepository
+        );
+    }
+
+    @Bean
+    public ErogarePremioControl erogarePremioControl(
+            SistemaPagamentoGateway sistemaPagamentoGateway,
+            HackathonRepository hackathonRepository
+    ) {
+        return new ErogarePremioControl(
+                sistemaPagamentoGateway,
+                hackathonRepository
+        );
+    }
+}
---
+++ src/test/java/io/github/filipp0o/hackhub/configuration/HackHubConfigurationTest.java
@@ -0,0 +1,92 @@
+package io.github.filipp0o.hackhub.configuration;
+
+import io.github.filipp0o.hackhub.application.ConfigurareRiscossionePremioControl;
+import io.github.filipp0o.hackhub.application.CreareHackathonControl;
+import io.github.filipp0o.hackhub.application.CreareTeamControl;
+import io.github.filipp0o.hackhub.application.ErogarePremioControl;
+import io.github.filipp0o.hackhub.application.EsaminareSegnalazioneControl;
+import io.github.filipp0o.hackhub.application.HackathonRepository;
+import io.github.filipp0o.hackhub.application.PartecipazioneRepository;
+import io.github.filipp0o.hackhub.application.ProclamareTeamVincitoreControl;
+import io.github.filipp0o.hackhub.application.SegnalareViolazioneControl;
+import io.github.filipp0o.hackhub.application.SegnalazioneRepository;
+import io.github.filipp0o.hackhub.application.SistemaPagamentoGateway;
+import io.github.filipp0o.hackhub.application.TeamRepository;
+import io.github.filipp0o.hackhub.application.UtenteRepository;
+import io.github.filipp0o.hackhub.application.ValutareSottomissioneControl;
+import io.github.filipp0o.hackhub.application.ValutazioneRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class HackHubConfigurationTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void configuraComponentiIterazioneUno() {
+        assertAll(
+                () -> assertNotNull(
+                        context.getBean(UtenteRepository.class)
+                ),
+                () -> assertNotNull(
+                        context.getBean(TeamRepository.class)
+                ),
+                () -> assertNotNull(
+                        context.getBean(PartecipazioneRepository.class)
+                ),
+                () -> assertNotNull(
+                        context.getBean(HackathonRepository.class)
+                ),
+                () -> assertNotNull(
+                        context.getBean(SegnalazioneRepository.class)
+                ),
+                () -> assertNotNull(
+                        context.getBean(ValutazioneRepository.class)
+                ),
+                () -> assertNotNull(
+                        context.getBean(SistemaPagamentoGateway.class)
+                ),
+                () -> assertNotNull(
+                        context.getBean(CreareTeamControl.class)
+                ),
+                () -> assertNotNull(
+                        context.getBean(CreareHackathonControl.class)
+                ),
+                () -> assertNotNull(
+                        context.getBean(
+                                ValutareSottomissioneControl.class
+                        )
+                ),
+                () -> assertNotNull(
+                        context.getBean(
+                                SegnalareViolazioneControl.class
+                        )
+                ),
+                () -> assertNotNull(
+                        context.getBean(
+                                EsaminareSegnalazioneControl.class
+                        )
+                ),
+                () -> assertNotNull(
+                        context.getBean(
+                                ProclamareTeamVincitoreControl.class
+                        )
+                ),
+                () -> assertNotNull(
+                        context.getBean(
+                                ConfigurareRiscossionePremioControl.class
+                        )
+                ),
+                () -> assertNotNull(
+                        context.getBean(ErogarePremioControl.class)
+                )
+        );
+    }
+}
