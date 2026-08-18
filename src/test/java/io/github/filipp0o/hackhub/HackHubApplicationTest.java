93d36ee902c83e632d408e212da63222e18203c8
Filipp0o <filippoferretti04@gmail.com>
2026-08-18 09:42:27 +0200

    Initialize Spring Boot application

---
+++ src/test/java/io/github/filipp0o/hackhub/HackHubApplicationTest.java
@@ -0,0 +1,12 @@
+package io.github.filipp0o.hackhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackHubApplicationTest {
+
+    @Test
+    void caricaContestoApplicativo() {
+    }
+}
